13101aa9eae771554d839e77714204fea34ffb5a
Alex Belov <be1ov@Alexs-MacBook-Air.local>
2024-04-27 15:37:10 +0300

    dick3

--- enitites.py
+++ enitites.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import declarative_base
 Base = declarative_base()
 
 
-class User(Base):
+class UserModel(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True, index=True)
     first_name = Column(String)
@@ -13,7 +13,7 @@ class User(Base):
     password = Column(String)
 
 
-class Project(Base):
+class ProjectModel(Base):
     __tablename__ = "projects"
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String)
--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
 
+import routes
+
 app = FastAPI(docs_url="/swagger", title="SmallBusinessApplication")
 
-
+app.include_router(routes.router)
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
