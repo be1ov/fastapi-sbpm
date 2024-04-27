f0a499728d9a488f078f623d943b1e1c36723121
Alex Belov <be1ov@Alexs-MacBook-Air.local>
2024-04-27 15:54:54 +0300

    pys fix

--- db.py
+++ db.py
@@ -1,7 +1,7 @@
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, Boolean
 
-SQLALCHEMY_DATABASE_URL = "postgresql+pyscopg2://sbpm:vv7Qs16L3jos@us-server.be1ov.ru:54320/SBPMApplication"
+SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://sbpm:vv7Qs16L3jos@us-server.be1ov.ru:54320/SBPMApplication"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
--- routes.py
+++ routes.py
@@ -1,8 +1,11 @@
 from fastapi import APIRouter
 
+import db
+from enitites import UserModel
+
 router = APIRouter()
 
 
 @router.get("/users", tags=["users"])
 async def get_all_users():
-    pass
+    return db.db.get(UserModel)
