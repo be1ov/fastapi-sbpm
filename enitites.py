de52fde9cae0184cc037858ad17008a61a81e355
arch1varius <112974779+arch1varius@users.noreply.github.com>
2024-04-27 15:49:21 +0300

    first

--- enitites.py
+++ enitites.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, String, Integer
+from sqlalchemy import Column, String, Integer, DateTime, ForeignKey
 from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
@@ -17,3 +17,19 @@ class Project(Base):
     __tablename__ = "projects"
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String)
+class TasksModel(Base):
+    __tablename__ = "tasks"
+    id = Column(Integer, primary_key=True, index=True)
+    project_id = Column(Integer, ForeignKey("projects.id"))
+    description = Column(String)
+    creator_id = Column(Integer, ForeignKey("users.id"))
+    controller_id = Column(Integer, ForeignKey("users.id"))
+    executor_id = Column(Integer, ForeignKey("users.id"))
+
+class TasksStatusesModel(Base):
+    __tablename__ = "task_statuses"
+    id = Column(Integer, primary_key=True, index=True)
+    date = Column(DateTime)
+    task_id = Column(Integer, ForeignKey("tasks.id"))
+    user_id = Column(Integer, ForeignKey("users.id"))
+    commentary = Column(String)
